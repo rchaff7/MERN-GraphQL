00587792dc9552a322fcabfa5d512cc52f5496d5
Ryan Chaffee <rchaff7@yahoo.com>
2021-07-30 00:16:57 -0500

    models created and Apollo sandbox is able to query data from MongoDB cloud

--- index.js
+++ index.js
@@ -1,18 +1,33 @@
 const { ApolloServer } = require('apollo-server');
 const gql = require('graphql-tag');
 const mongoose = require('mongoose');
+
 const { MONGODB } = require('./config');
+const Post = require('./models/Post');
 
 const typeDefs = gql` 
+    type Post{
+        id: ID!
+        body: String!
+        createdAt: String!
+        username: String!
+    }
     type Query{
-        sayHi: String!
+        getPosts: [Post]
     }
 
 `;
 
 const resolvers = {
     Query: {
-        sayHi: () => "Hello World!"
+        async getPosts(){
+            try{
+                const posts = await Post.find();
+                return posts;
+            } catch (err) {
+                throw new Error(err);
+            }
+        }
 
     }
 };
